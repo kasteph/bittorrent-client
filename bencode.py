54c2ab0df2a9ffecf5775f4828c8db903e8f58c5
Steph Samson <sdvsamson@gmail.com>
2014-10-30 20:08:48 -0400

    get_int and more tests for get_string

--- bencode.py
+++ bencode.py
@@ -1,19 +1,40 @@
-isa = isinstance
+'''
+The <data> parameter in the functions below are
+bencoded strings with type <str> in Python.
+'''
 
 
 def get_string(data):
     '''
-    Where [data] is a string. A valid bencoded
-    string is in the format of x:y, where x is
-    an int and y the contents of the string.
+    A valid bencoded string is in the format of x:y,
+    where x is an int and y the contents of the string.
 
     get_string assumes that if the first element
     of data cannot be type converted into an int,
     then it is not a valid bencoded string.
     '''
-    data = data.split(':')
+    data = data.split(':', 1)
     try:
-        data[0] = int(data[0])
+        length = int(data[0])
     except ValueError:
         raise ValueError('Not a valid bencoded string.')
-    return data[1]
+    wanted_string = data[1][:length]
+    remainder = data[1][length:]
+    return wanted_string, remainder
+
+
+def get_int(data):
+    '''
+    A valid bencoded integer is in the format ixe,
+    where x is an integer, and i and e are the delimiters.
+    '''
+    return int(data[1:data.index('e')])
+
+
+def get_list(data):
+    '''
+    A valid bencoded list is in the format l<bencoded_vals>e,
+    where l and e are the delimiters. Lists can contain any bencoded
+    type.
+    '''
+    pass
--- bencode_tests.py
+++ bencode_tests.py
@@ -1,15 +1,23 @@
 import unittest
-from bencode import get_string
+from bencode import get_string, get_int, get_list
 
 
 class TestBencode(unittest.TestCase):
 
     def test_get_string(self):
-        self.assertEqual(get_string('4:spam'), 'spam')
+        self.assertEqual(get_string('4:spam'), ('spam', ''))
+        self.assertEqual(get_string('6:foobar'), ('foobar', ''))
+        self.assertEqual(get_string('5:hello3:heyi23e'), ('hello', '3:heyi23e'))
+        self.assertEqual(get_string('3:foobaz:i4e'), ('foo', 'baz:i4e'))
 
     def test_get_string_error(self):
-        self.assertRaises(ValueError, get_string, 'e4:bar')
+        self.assertRaises(ValueError, get_string, 'e3:bar')
 
     def test_get_int(self):
         self.assertEqual(get_int('i3e'), 3)
+        self.assertEqual(get_int('i-3e'), -3)
+        self.assertEqual(get_int('i04e'), 4)
+        self.assertEqual(get_int('i0e'), 0)
 
+    def test_get_list(self):
+        self.assertEqual(get_list('l4:spam4:eggse'), ['spam', 'eggs'])
