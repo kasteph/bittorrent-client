4e625bccb0a9f22a3d4856fdb5cf54531136b251
Steph Samson <sdvsamson@gmail.com>
2014-11-03 12:12:14 -0500

    startdecode bencoding

--- bencode.py
+++ bencode.py
@@ -16,6 +16,7 @@ def get_string(data):
     data = data.split(':', 1)
     try:
         length = int(data[0])
+        assert isinstance(length, int)
     except ValueError:
         raise ValueError('Not a valid bencoded string.')
     wanted_string = data[1][:length]
@@ -28,6 +29,7 @@ def get_int(data):
     A valid bencoded integer is in the format ixe,
     where x is an integer, and i and e are the delimiters.
     '''
+    assert data[0] == 'i'
     return int(data[1:data.index('e')])
 
 
@@ -37,4 +39,29 @@ def get_list(data):
     where l and e are the delimiters. Lists can contain any bencoded
     type.
     '''
+    assert data[0] == 'l'
+    return
+
+
+def get_dict(data):
     pass
+
+
+def bdecode(string):
+    first = string[0]
+    if first in '0123456789':
+        wanted_string, remainder_string = get_string(string)
+        return wanted_string, remainder_string
+    if first == 'i':
+        return get_int(string)
+    if first == 'l':
+        bdecoded_list = []
+        for c in string:
+            print(c)
+            bdecoded_list.append(bdecode(string[1:]))
+    if first == 'e':
+        return bdecoded_list
+    if first == 'd':
+        bdecoded_dict = []
+        bdecoded_dict.append(bdecoded_dict)
+    return bdecode(string[1:])
--- bencode_tests.py
+++ bencode_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from bencode import get_string, get_int, get_list
+from bencode import get_string, get_int, bdecode
 
 
 class TestBencode(unittest.TestCase):
@@ -7,7 +7,7 @@ class TestBencode(unittest.TestCase):
     def test_get_string(self):
         self.assertEqual(get_string('4:spam'), ('spam', ''))
         self.assertEqual(get_string('6:foobar'), ('foobar', ''))
-        self.assertEqual(get_string('5:hello3:heyi23e'), ('hello', '3:heyi23e'))
+        self.assertEqual(get_string('5:hello3:heyi2e'), ('hello', '3:heyi2e'))
         self.assertEqual(get_string('3:foobaz:i4e'), ('foo', 'baz:i4e'))
 
     def test_get_string_error(self):
@@ -20,4 +20,4 @@ class TestBencode(unittest.TestCase):
         self.assertEqual(get_int('i0e'), 0)
 
     def test_get_list(self):
-        self.assertEqual(get_list('l4:spam4:eggse'), ['spam', 'eggs'])
+        self.assertEqual(bdecode('l4:spam4:eggse'), ['spam', 'eggs'])
