aa968399ee835d96efe206cb7d4bff2cc8a74513
Steph Samson <sdvsamson@gmail.com>
2014-11-04 17:55:36 -0500

    Connecting to one peer

--- torrent.py
+++ torrent.py
@@ -1,6 +1,7 @@
 import bencode
 import hashlib
 import struct
+import socket
 import requests
 
 
@@ -15,9 +16,11 @@ class Torrent(object):
         self._get_torrent_info()
 
     def _get_torrent_info(self):
-        self._torrent_dict = bencode.bdecode(open('tom.torrent').read())
+        self._torrent_dict = bencode.bdecode(open(self.torrent_file).read())
+        print self._torrent_dict.keys()
         self._info = self._torrent_dict['info']
         self.announce_url = self._torrent_dict['announce']
+        print self.announce_url
         self.length = self._info['length']
         self.piece_length = self._info['piece length']
         self.name = self._info['name']
@@ -35,10 +38,10 @@ class Tracker(object):
         return bencode.bdecode(response.content)
 
     def _build_payload(self):
-        self.info_hash = hashlib.sha1(self.torrent.bencoded_info)
+        self.info_hash = hashlib.sha1(self.torrent.bencoded_info).digest()
         self.left = self.torrent.length
         payload = {
-            'info_hash': self.info_hash.digest(),
+            'info_hash': self.info_hash,
             'peer_id': self.peer_id,
             'left': self.left
         }
@@ -50,7 +53,7 @@ class Tracker(object):
         peers = []
         if type(peer_data) == str:
             while byte_offset < len(peer_data):
-                peer = struct.unpack_from('BBBBH', peer_data, byte_offset)
+                peer = struct.unpack_from('!BBBBH', peer_data, byte_offset)
                 peers.append(peer)
                 byte_offset += 6
         peers = [('.'.join(repr(i) for i in peer[0:4]), peer[4]) for peer in peers]
@@ -58,9 +61,24 @@ class Tracker(object):
 
 
 def main():
-    torrent = Torrent()
+    torrent = Torrent('tom.torrent')
     tracker = Tracker(torrent)
-    print tracker.get_peers()
+    print tracker.get_peers()[1]
+    my_peer = tracker.get_peers()[1]
+    # CONNECT TO PEER
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(my_peer)
+    pstr = 'BitTorrent protocol'
+    pstrlen = len(pstr)
+    res = '\x00' * 8
+    info_hash = tracker.info_hash
+    peer_id = tracker.peer_id
+    MESSAGE = struct.pack('B', pstrlen)+pstr+res+info_hash+peer_id
+    s.send(MESSAGE)
+    data = s.recv(1024)
+    s.close()
+
+    print data
 
 
 if __name__ == '__main__':
